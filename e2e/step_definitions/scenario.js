ea3a962d95a15aef38c45f02a1fa558f7fd608dd
dzaja123 <nikolacardzajevic@gmail.com>
2022-10-18 09:34:02 +0200

    Added scenario.feature

--- e2e/step_definitions/scenario.js
+++ e2e/step_definitions/scenario.js
@@ -2,85 +2,85 @@ const {Given, When, Then, And} = require('cucumber');
 const axios = require('axios');
 
 
-When(/^A creates a new user$/, () => {
+When('A creates a new user', () => {
 
 
 });
 
 
-Then(/^A new user will be added to the database$/, () => {
+Then('A new user will be added to the database', () => {
 
 });
 
 
 
-When(/^A user checks that the user is created correctly$/, () => {
+When('A user checks that the user is created correctly', () => {
 
 
 }); 
 
 
-Then(/^A the data from the database will be correct$/, () => {
+Then('A the data from the database will be correct', () => {
 
 
 }); 
 
 
-When(/^A user updates his email data$/, () => {
+When('A user updates his email data', () => {
 
 
 });
 
 
-When(/^A user verifies that the update was successfull$/, () => {
+When('A user verifies that the update was successfull', () => {
 
 
 }); 
   
 
-Then(/^A user will be able to see the record$/, () => {
+Then('A user will be able to see the record', () => {
 
 
 });
   
 
-When(/^A user tries to login$/, () => {
+When('A user tries to login', () => {
 
 
 });
   
 
-Then(/^A user will be loged in successsfully$/, () => {
+Then('A user will be loged in successsfully', () => {
 
 
 }); 
   
 
-When(/^A user tries to logout$/, () => {
+When('A user tries to logout', () => {
 
 
 });
   
 
-Then(/^A user will be logged out$/, () => {
+Then('A user will be logged out', () => {
 
 
 }); 
   
 
-When(/^A user tries to delete his account$/, () => {
+When('A user tries to delete his account', () => {
 
 
 }); 
   
 
-When(/^A user verifies that the account is deleted from the database$/, () => {
+When('A user verifies that the account is deleted from the database', () => {
 
 
 }); 
   
 
-Then(/^A user will be deleted from the database$/, () => {
+Then('A user will be deleted from the database', () => {
 
 
 });
