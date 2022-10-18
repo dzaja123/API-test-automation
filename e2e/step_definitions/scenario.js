e4697d6a1a3a0ac810776f784fa4100e610b1942
dzaja123 <nikolacardzajevic@gmail.com>
2022-10-18 09:30:09 +0200

    Added scenario.feature

---
+++ e2e/step_definitions/scenario.js
@@ -0,0 +1,87 @@
+const {Given, When, Then, And} = require('cucumber');
+const axios = require('axios');
+
+
+When(/^A creates a new user$/, () => {
+
+
+});
+
+
+Then(/^A new user will be added to the database$/, () => {
+
+});
+
+
+
+When(/^A user checks that the user is created correctly$/, () => {
+
+
+}); 
+
+
+Then(/^A the data from the database will be correct$/, () => {
+
+
+}); 
+
+
+When(/^A user updates his email data$/, () => {
+
+
+});
+
+
+When(/^A user verifies that the update was successfull$/, () => {
+
+
+}); 
+  
+
+Then(/^A user will be able to see the record$/, () => {
+
+
+});
+  
+
+When(/^A user tries to login$/, () => {
+
+
+});
+  
+
+Then(/^A user will be loged in successsfully$/, () => {
+
+
+}); 
+  
+
+When(/^A user tries to logout$/, () => {
+
+
+});
+  
+
+Then(/^A user will be logged out$/, () => {
+
+
+}); 
+  
+
+When(/^A user tries to delete his account$/, () => {
+
+
+}); 
+  
+
+When(/^A user verifies that the account is deleted from the database$/, () => {
+
+
+}); 
+  
+
+Then(/^A user will be deleted from the database$/, () => {
+
+
+});
+  
